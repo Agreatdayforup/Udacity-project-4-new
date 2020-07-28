75616fa28dd92f2cb72d9d7418756f0214aa7e2c
Patrick <cayerp@gmail.com>
2020-07-28 15:01:30 -0400

    adjust links

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ console.log('the client side is loaded');
 
 import { handleSubmit } from './src/js/formSub.js'
 
-import './src/styles/base.scss';
+import './styles/base.scss';
 //import './styles/footer.scss'
 //import './styles/form.scss'
 //import './styles/header.scss'
