bc9228a79b1bc0b4750a4ff07f7282c55fc5f152
Patrick C <35810793+Agreatdayforup@users.noreply.github.com>
2020-07-28 13:39:50 -0400

    reconnect link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 console.log('the client side is loaded');
 
-import { handleSubmit } from './js/formSub.js'
+import { handleSubmit } from './src/js/formSub.js'
 
-import './styles/base.scss';
+import './src/styles/base.scss';
 //import './styles/footer.scss'
 //import './styles/form.scss'
 //import './styles/header.scss'
